3c12f26c2f47bbaccdb271106e5e261b17d06827
rminosso <renan.msilva@sptech.school>
2026-02-19 15:07:59 -0300

    comeco implementacao cadastro/login

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -7,8 +7,7 @@ function buscarPorId(id) {
 }
 
 function listar() {
-  var instrucaoSql = `SELECT id, razao_social, cnpj, codigo_ativacao FROM empresa`;
-
+  var instrucaoSql = `SELECT idEmpresa FROM empresa`;
   return database.executar(instrucaoSql);
 }
 
