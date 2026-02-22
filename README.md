0d6dc895168dae6e4f002cf5f2c48def20b1a9ea
MarleyS439 <marleysantos439@gmail.com>
2026-02-22 14:40:36 -0300

    Melhoria e correções em classes e estilizações css.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Atmos
+# Atmos Monitoring System
+
+Atmos é um Sistema de Monitoramento de Servidores
+
+## Sobre
+
+## Tecnologias
+
+### Front-end
+
+- HTML
+- CSS
+- JavaScript
+
+### Back-end
+
+- NodeJS
+- MySQL
+
+## Instalação
+
+## Contribuição
+
+## Licença e Autores
+
+Este projeto foi idealizado e desenvolvido pelos seguintes autores:
+
+- Geovanna V. de Oliveira ([@geovanna-v ](https://github.com/geovanna-v))
+
+- Guilherme Ornaghi ([@Ornaghiii ](https://github.com/Ornaghiii))
+
+- Guilherme S. Souza ([@GuilhermeSoutoS](https://github.com/GuilhermeSoutoS))
+
+- Marley de S. Santos ([@MarleyS439](https://github.com/MarleyS439))
+
+- Renan M. Silva ([@rminosso](https://github.com/rminosso))
+
+- Samara V. da S. Mendonça ([@SamaraMendoncavic](https://github.com/SamaraMendoncavic))
+
--- public/assets/javascript/chat.js
+++ public/assets/javascript/chat.js
@@ -1,12 +1,55 @@
 // Adiciona um evento ao ícone de chat para quando ser clicado, abrir o chat
 const chatIcon = document.getElementById('chat');
 const chatBox = document.getElementById('conversation');
+const sendButton = document.getElementById('send');
+const closeChat = document.getElementById('close-chat');
+
+
+// Fecha o chat pelo ícone de X
+closeChat.addEventListener('click', () => {
+    chatBox.style.display = 'none';
+})
 
 // Adiciona um ouvinte de clique
 chatIcon.addEventListener('click', () => {
-    if (chatBox.style.display == 'none') {
+    if (chatBox.style.display === 'none') {
         chatBox.style.display = 'flex';
     } else {
         chatBox.style.display = 'none';
     }
 });
+
+
+
+// Coleta os dados da mensagem e envia
+sendButton.addEventListener('click', () => {
+
+    let dia = new Date().getDate();
+    let mesTexto = new Date().toLocaleString('pt-BR', { month: 'long' });
+    let hora = new Date().getHours();
+    let minutos = new Date().getMinutes();
+
+    let mesAbreviado = mesTexto.substring(0, 3);
+
+    let dataHora = `${dia} ${mesAbreviado}., ${hora}:${minutos}`;
+
+    let contentInput = document.getElementById('mensagem');
+
+    if (contentInput.value === '') {
+        sendButton.disable();
+    } else {
+        document.getElementById('chat-conversation').innerHTML += `
+            <div class="baloon-user">
+                <span>${contentInput.value}</span>
+            </div>
+            <span class="date-time-user">${dataHora}</span>
+        `;
+
+        contentInput.value = "";
+    }
+
+})
+
+// Função para enviar a mensagem para a AI (Gemini)
+function sendMessageToAi() {}
+
