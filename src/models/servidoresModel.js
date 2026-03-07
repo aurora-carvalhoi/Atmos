5f5a0123c03b411ebe7f84ac8fcd66121735da4b
GuilhermeSoutoS <guilherme.ssouza@sptech.school>
2026-03-07 01:28:22 -0300

    Back-end cadastro de servidores

---
+++ src/models/servidoresModel.js
@@ -0,0 +1,18 @@
+var database = require("../database/config")
+
+
+function cadastrar(nomeServidor, numeroIdentificacao, sistemaOperacional,enderecoIP, fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nomeServidor, numeroIdentificacao, sistemaOperacional,enderecoIP, fkEmpresa);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO servidor (fkEmpresa, nome, numeroIdentificacao, sistemaOperacional,enderecoIP) VALUES (${fkEmpresa},'${nomeServidor}','${numeroIdentificacao}','${sistemaOperacional}','${enderecoIP}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    cadastrar
+};
---
+++ src/routes/servidores.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+
+var servidoresController = require("../controllers/servidoresController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    servidoresController.cadastrar(req, res);
+})
+
+module.exports = router;
