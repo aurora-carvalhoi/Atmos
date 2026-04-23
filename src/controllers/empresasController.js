8e90e4308a0bccb3f600977cfa0cba0acd475fa5
ribeirokk <luiz.ribeiro@sptech.school>
2026-04-23 02:55:34 -0300

    Atualização do BackEnd conforme o novo script MySql + BackEnd Cadastro Empresa funcionando

--- src/controllers/empresasController.js
+++ src/controllers/empresasController.js
@@ -18,8 +18,29 @@ function listarEmpresasCadastradas(req, res){
     })
 }
 
+function cadastrar(req, res) {
+    var { 
+        nome, responsavel, cnpj, email, telefoneCelular, 
+        cep, logradouro, bairro, cidade, uf, numero, 
+        status, ddd 
+    } = req.body;
+
+    if (!nome || !cnpj || !email || !cep || !numero) {
+        res.status(400).send("Campos obrigatórios não preenchidos (Nome, CNPJ, Email, CEP ou Número)");
+    } else {
+        empresaModel.cadastrar(nome, responsavel, cnpj, email, telefoneCelular, cep, logradouro, bairro, cidade, uf, numero, status, ddd)
+            .then((resultado) => {
+                res.status(201).json(resultado);
+            }).catch((erro) => {
+                console.log(erro);
+                res.status(500).json(erro.sqlMessage);
+            });
+    }
+}
+
 module.exports = {
     listar,
     listarRecentes,
-    listarEmpresasCadastradas
+    listarEmpresasCadastradas,
+    cadastrar
 }
--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,28 +1,53 @@
 const database = require("../database/config")
 
 function listar(){
-    var instrucaoSql = "SELECT * FROM cadastroEmpresa";
+    var instrucaoSql = "SELECT * FROM empresa";
     return database.executar(instrucaoSql);
 }
 
 function listarRecentes(){
-    var instrucaoSql = `    SELECT idcadastroEmpresa AS idEmpresa, nomeResponsavel, nomeEmpresa, statusCliente, cidade, uf
-    FROM cadastroEmpresa LEFT JOIN endereco ON idcadastroEmpresa = fkEmpresa
+    var instrucaoSql = `    SELECT idEmpresa, nomeResponsavel, razaoSocial, statusEmpresa, cidade, uf
+    FROM empresa LEFT JOIN endereco ON idEmpresa = fkEmpresa
     order by idEmpresa desc limit 3; `;
     return database.executar(instrucaoSql);
 }
 
 function listarEmpresasCadastradas(){
     var instrucaoSql = `
-    SELECT idcadastroEmpresa, nomeEmpresa, nomeResponsavel, cnpj, telefoneFixo, email, statusCliente
-    FROM cadastroEmpresa LEFT JOIN contato on idcadastroEmpresa = fkEmpresa
-    order by idcadastroEmpresa desc; 
+    SELECT idEmpresa, razaoSocial, nomeResponsavel, cnpj, telefoneCelular, email, statusEmpresa
+    FROM empresa LEFT JOIN contato on idEmpresa = fkEmpresa
+    order by idEmpresa desc; 
     `;
     return database.executar(instrucaoSql);
+}
+
+function cadastrar(nome, responsavel, cnpj, email, telefoneCelular, cep, logradouro, bairro, cidade, uf, numero, status, ddd) {
+    var instrucaoSqlEmpresa = `
+        INSERT INTO empresa (nomeResponsavel, razaoSocial, cnpj, senha, statusEmpresa) 
+        VALUES ('${responsavel}', '${nome}', '${cnpj}', '123456', '${status}');
+    `;
+
+    return database.executar(instrucaoSqlEmpresa).then((resultado) => {
+        const idEmpresa = resultado.insertId;
+
+        var instrucaoSqlContato = `
+            INSERT INTO contato (idContato, fkEmpresa, DDD, telefoneFixo, telefoneCelular, email) 
+            VALUES (1, ${idEmpresa}, '${ddd}', 'null', '${telefoneCelular}', '${email}');
+        `;
+
+        var instrucaoSqlEndereco = `
+            INSERT INTO endereco (cep, logradouro, numero, bairro, cidade, uf, fkEmpresa)
+            VALUES ('${cep}', '${logradouro}', ${numero}, '${bairro}', '${cidade}', '${uf}', ${idEmpresa});
+        `;
+
+        database.executar(instrucaoSqlContato);
+        return database.executar(instrucaoSqlEndereco);
+    });
 }
 
 module.exports = {
     listar,
     listarRecentes,
-    listarEmpresasCadastradas
-}
+    listarEmpresasCadastradas,
+    cadastrar
+};
