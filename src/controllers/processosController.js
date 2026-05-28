25f6676835d33aec744cc1ff5d44f971dfa3270f
Kaio-rocha-sptech <kaio.rocha@sptech.school>
2026-05-28 10:46:33 -0300

    Adição nome empresa na sessions storage

--- src/controllers/processosController.js
+++ src/controllers/processosController.js
@@ -1,11 +1,43 @@
-const procesosModel = require("../models/processoModel")
+const procesosModel = require("../models/processoModel");
+const s3Service = require("../services/s3Services");
+
 
 function buscar(req, res){
-    return "ok!";
+    res.json({
+        "message": "funcionou!"
+    });
+}
+
+
+async function listarProcessos(req, res) {
+
+  try {
+
+    const key =
+      "client/empresaX/snapshot_1779932998.json";
+
+    const dados = await s3Service.buscarJson(
+      process.env.BUCKET_NAME,
+      key
+    );
+
+    res.json(dados);
+
+  } catch (erro) {
+
+    console.error(erro);
+
+    res.status(500).json({
+      erro: "Erro ao buscar processos"
+    });
+
+  }
+
 }
 
 
 
 module.exports = {
-    buscar
+    buscar,
+    listarProcessos
 }
