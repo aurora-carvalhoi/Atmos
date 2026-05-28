1834732bb430522ff8f854f95463e47d8b09ca09
Kaio-rocha-sptech <kaio.rocha@sptech.school>
2026-05-28 17:19:44 -0300

    tornando dashboard processos semi dinamica

--- src/routes/processos.js
+++ src/routes/processos.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const processosController = require("../controllers/processosController")
 
-router.get("/",processosController.buscar)
 router.get("/listar", processosController.listarProcessos);
 
 module.exports = router;
